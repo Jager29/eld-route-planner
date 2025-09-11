a398a854fea3d050838b1708393d155c021dd787
Jager29 <jmsuira2911@hotmail.com>
2025-09-10 21:57:01 -0500

    PREP

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -4,13 +4,19 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 SECRET_KEY = os.getenv("SECRET_KEY", "change-me")
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
-INSTALLED_APPS += [
+INSTALLED_APPS = [
     "corsheaders",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
-    "corsheaders.middleware.CorsMiddleware",
-    *MIDDLEWARE,
+    "corsheaders.middleware.CorsMiddleware",        
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",    
+    "django.middleware.common.CommonMiddleware",
 ]
+
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 CORS_ALLOW_ALL_ORIGINS = os.getenv("CORS_ALLOW_ALL_ORIGINS", "True") == "True"
